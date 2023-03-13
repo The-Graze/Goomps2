e2fd9a04f0cdb928bcf259f84bfb205a8c5f6ae1
Graze <Graze@DESKTOP-OKRBILU>
2023-03-13 07:07:49 +0000

    Add project files.

---
+++ Goops2/Plugin.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx;
+using Photon.Pun;
+using System;
+using System.Linq;
+using UnityEngine;
+using Utilla;
+
+namespace Goops2
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        GOOMPS2 g2;
+        void Start()
+        {Utilla.Events.GameInitialized += OnGameInitialized;}
+
+        void OnEnable()
+        {
+            g2.enabled = true;
+        }
+
+        void OnDisable()
+        {
+            g2.enabled = false;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            g2 = GameObject.Find("Local Gorilla Player").AddComponent<GOOMPS2>();
+            GorillaParent.instance.vrrigParent.AddComponent<Adder>();
+        }
+    }
+    public class GOOMPS2 : MonoBehaviour
+    {
+        void Awake()
+        {
+
+        }
+        void Update()
+        {
+            GameObject[] objs = GameObject.FindObjectsOfType(typeof(GameObject)).Select(g => g as GameObject).Where(g => g.GetComponent<Gplayer>() != null).ToArray();
+            foreach (GameObject g in objs)
+            {
+                if(Vector3.Distance(transform.position, g.transform.position) < 0.52f)
+                {
+                    g.transform.GetChild(3).GetComponent<Renderer>().enabled = false;
+                }
+                else
+                {
+                    g.transform.GetChild(3).GetComponent<Renderer>().enabled = true;
+                }
+            }
+        }
+    }
+    class Gplayer : MonoBehaviour
+    {
+
+    }
+
+
+    public class Adder : MonoBehaviour
+    {
+        void LateUpdate()
+        {
+            foreach (Transform child in transform)
+            {
+                if (child.gameObject.GetComponent<Gplayer>() == null && child.GetComponent<PhotonView>().Owner != PhotonNetwork.LocalPlayer)
+                {
+                    child.gameObject.AddComponent<Gplayer>();
+                }
+            }
+        }
+    }
+}
---
+++ Goops2/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Goops2
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.goomps2";
+        public const string Name = "Goomps2";
+        public const string Version = "1.0.0";
+    }
+}
